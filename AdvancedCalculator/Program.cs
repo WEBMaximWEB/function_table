49bfc3cc876bf0d87afeb942ee7c613699b09461
WEBMaximWEB <maxim.geer@yandex.ru>
2021-03-06 09:57:12 +0500

    avalonia

--- AdvancedCalculator/Program.cs
+++ AdvancedCalculator/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using FunctionBuilder.Logic;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using AdvancedCalculator;
 
 namespace function_table
 {
@@ -10,8 +9,8 @@ namespace function_table
     {
         static void Main(string[] args)
         {
-            string str = Rpn.StartRpn();
-            WriteInFile(Sheet.StartSheet(str));
+            string str = Rpn.StartRpn(ReadFlile());
+            WriteSheet(str);
         }
 
         static void WriteInFile(List<string> sheet)
@@ -22,6 +21,46 @@ namespace function_table
                 for(int i = 0; i < sheet.Count; i++)
                     sw.WriteLine(sheet[i]);
             }
+        }
+
+        private static string ReadFlile()
+        {
+            string line = "";
+            int counter = 0;
+
+            StreamReader f = new StreamReader(@"intput.txt");
+            while (line == "" && counter < 20)
+            {
+                line += f.ReadLine();
+                counter++;
+            }
+            f.Close();
+            return line;
+        }
+
+        private static void WriteSheet(string str)
+        {
+            double steps, xMin, xMax;
+            steps = Check("шаг функции");
+            xMin = Check("минимальное значение");
+            xMax = Check("минимальное значение");
+
+            WriteInFile(Sheet.StartSheet(str, steps, xMin, xMax));
+        }
+
+        private static float Check(string text)
+        {
+            //проверка введенных значений
+            string input;
+            float output;
+            Console.Write("Укажите " + text + ":");
+            input = Console.ReadLine();
+            while (!float.TryParse(input, out output))
+            {
+                Console.Write("Вы допустили ошибку! Введите значение повторно:");
+                input = Console.ReadLine();
+            }
+            return output;
         }
     }
 }
